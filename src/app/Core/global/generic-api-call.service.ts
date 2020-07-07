37498f8ec33da86dbbb9a44fa13aa74b9985fb5c
Felix Campoa <felixcampoa@gmail.com>
2020-07-07 09:25:05 -0700

    metodos de pago modal

--- src/app/Core/global/generic-api-call.service.ts
+++ src/app/Core/global/generic-api-call.service.ts
@@ -3,6 +3,7 @@ import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 
+
 @Injectable({
   providedIn: 'root'
 })
