eb1f05228814175ba88889885c12f84510444624
Felix Campoa <felixcampoa@gmail.com>
2020-07-20 15:00:30 -0700

    backup

--- src/app/components/payment/add-payment/add-payment.component.ts
+++ src/app/components/payment/add-payment/add-payment.component.ts
@@ -51,11 +51,13 @@ export class AddPaymentComponent implements OnInit {
   }
 
   patchValues(): void {
+    debugger;
+    const d = new Date(this.data.vigencia);
     this.group.patchValue({
       txtTitular: this.data.titular,
       txtTarjeta: this.data.numero_tarjeta,
-      selectMes: this.meses[this.data.vigencia.getMonth()],
-      selectAnho: this.data.vigencia.getFullYear(),
+      selectMes: this.meses[d.getMonth() + 1],
+      selectAnho: this.anhos[this.anhos.indexOf(d.getFullYear().toString())],
       txtCVV: this.data.cvv
     });
   }
