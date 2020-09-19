18c40969ecd9f793b413c8daa8ace560c3441337
felix daniel campoa toledo <felixcampoa@gmail.com>
2020-09-19 14:54:11 -0700

    seguridad

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,8 @@
 export const environment = {
   production: true,
   // base_url: 'http://142.93.59.159:8515/beatstudio/'
-   base_url: 'http://165.22.91.247:8116/beatstudio/'
+  // base_url: 'http://165.22.91.247:8116/beatstudio/'
   // base_url: 'https://165.22.91.247:8116/beatstudio/'
   // base_url: '//165.22.91.247:8116/beatstudio/'
+  base_url: 'www.beatstudio.com.mx/directus/beatstudio/'
 };
