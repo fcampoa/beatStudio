14b8c2973da257f8aab411b3419def33306626c7
Felix Campoa <felixcampoa@gmail.com>
2020-09-18 18:01:27 -0700

    url segura al back

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: true,
   // base_url: 'http://142.93.59.159:8515/beatstudio/'
-  base_url: 'http://165.22.91.247:8116/beatstudio/'
+  // base_url: 'http://165.22.91.247:8116/beatstudio/'
+  base_url: 'https://165.22.91.247:8116/beatstudio/'
+
 };
