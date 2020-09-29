89beef2f330bd20f5659239fc65817be84566239
Felix Campoa <felixcampoa@gmail.com>
2020-09-28 19:29:49 -0700

    endpoints correos

--- src/app/services/file-service.service.ts
+++ src/app/services/file-service.service.ts
@@ -21,41 +21,41 @@ export class FileService {
     private crearTablaLista(lista: any, horario: Horario): any {
         return {
             // content: 'lista de clase ' + horario.disciplina.nombre + '  hora:' + horario.fecha ,
-                // {
-                //     text: 'clase: ' + horario.disciplina.nombre + '  hora:' + horario.fecha + '  coach: ' + horario.coach.nombre,
-                //     bold: true,
-                //     fontSize: 20,
-                //     alignment: 'center',
-                //     margin: [0, 0, 0, 20]
-                // },
+            // {
+            //     text: 'clase: ' + horario.disciplina.nombre + '  hora:' + horario.fecha + '  coach: ' + horario.coach.nombre,
+            //     bold: true,
+            //     fontSize: 20,
+            //     alignment: 'center',
+            //     margin: [0, 0, 0, 20]
+            // },
             table: {
                 widths: ['*', '*', '*'],
-                    body: [
-                        [
-                            {
-                                text: 'Nombre',
-                                style: 'tableHeader'
-                            },
-                            {
-                                text: 'Lugar',
-                                style: 'tableHeader'
-                            }, {
-                                text: 'Firma',
-                                style: 'tableHeader'
-                            }
-                        ],
-                        ...lista.map(x => {
-                            return [x.nombre, x.lugar, '____________________'];
-                        })
-                    ]
+                body: [
+                    [
+                        {
+                            text: 'Nombre',
+                            style: 'tableHeader'
+                        },
+                        {
+                            text: 'Lugar',
+                            style: 'tableHeader'
+                        }, {
+                            text: 'Firma',
+                            style: 'tableHeader'
+                        }
+                    ],
+                    ...lista.map(x => {
+                        return [x.nombre, x.lugar, '____________________'];
+                    })
+                ]
             }
         };
 
-}
+    }
 
-exportAsPDF(data: any) {
-    const documentDefinition = { content: data };
-    pdfMake.createPdf(documentDefinition).download();
-}
+    exportAsPDF(data: any) {
+        const documentDefinition = { content: data };
+        pdfMake.createPdf(documentDefinition).download();
+    }
 
 }
