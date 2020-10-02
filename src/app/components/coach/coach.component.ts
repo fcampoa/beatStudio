6dabb2cbaf7176b83ffc46242272c9f7c40e4703
alvarov1 <alvarox3a@gmail.com>
2020-10-01 19:35:57 -0700

    coach alexa

--- src/app/components/coach/coach.component.ts
+++ src/app/components/coach/coach.component.ts
@@ -92,53 +92,23 @@ export class CoachComponent implements OnInit {
       nombre: 'Marie',
       url: '../../../assets/img/new-images/Coaches/Coach_Marielena.jpg',
       musica: 'Oldies'
+    },
+    {
+      frase: 'No one is you and that is your power',
+      disciplinas: 'BeatSpin',
+      movimiento: 'Sprint / No hands',
+      nombre: 'Alexa Caliz',
+      url: '../../../assets/img/new-images/Coaches/Coach_Alexa.jpg',
+      musica: 'Desde Bad Bunny hasta Kayne West'
     }
 
   ];
+  
   constructor(private builder: FormBuilder, private titleService: Title) {
-    window.scrollTo(0,0);
+    window.scrollTo(0, 0);
     this.titleService.setTitle('Coaches — BeatStudio');
   }
 
   ngOnInit() {
-    // this.group = this.builder.group({});
-    // this.coaches.forEach((x, i) => {
-    //   this.group.addControl(i + 'name', new FormControl({ value: x.nombre, disabled: true }));
-    //   this.group.addControl(i + 'music', new FormControl({ value: x.musica, disabled: true }));
-    // });
   }
-
-  // showDetails(i: number) {
-  //   const el = document.getElementById(String(i));
-  //   el.classList.remove('front-image');
-  //   el.classList.add('img-size');
-  //   const div = document.getElementById(String(i) + 'divider');
-  //   div.classList.remove('divider-class-hide');
-  //   div.classList.add('divider-class-show');
-  //   this.setValue(i, this.coaches[i].nombre + ' ' + this.coaches[i].apellido);
-  //   document.getElementById(String(i) + 'coachInfo').classList.remove('coach-info-hide');
-  //   document.getElementById(String(i) + 'coachInfo').classList.add('coach-info-show');
-  //   document.getElementById(String(i) + 'name').classList.remove('coach-name-music-white');
-  //   document.getElementById(String(i) + 'music').classList.remove('coach-name-music-white');
-  //   document.getElementById(String(i) + 'name').classList.add('coach-name-music-purple');
-  //   document.getElementById(String(i) + 'music').classList.add('coach-name-music-purple');
-  // }
-  // hideDetails(i: number) {
-  //   const el = document.getElementById(String(i));
-  //   el.classList.remove('img-size');
-  //   el.classList.add('front-image');
-  //   const div = document.getElementById(String(i) + 'divider');
-  //   div.classList.remove('divider-class-show');
-  //   div.classList.add('divider-class-hide');
-  //   this.setValue(i, this.coaches[i].nombre);
-  //   document.getElementById(String(i) + 'coachInfo').classList.remove('coach-info-show');
-  //   document.getElementById(String(i) + 'coachInfo').classList.add('coach-info-hide');
-  //   document.getElementById(String(i) + 'name').classList.remove('coach-name-music-purple');
-  //   document.getElementById(String(i) + 'music').classList.remove('coach-name-music-purple');
-  //   document.getElementById(String(i) + 'name').classList.add('coach-name-music-white');
-  //   document.getElementById(String(i) + 'music').classList.add('coach-name-music-white');
-  // }
-  // setValue(i: number, value: string) {
-  //   this.group.controls[String(i) + 'name'].setValue(value);
-  // }
 }
