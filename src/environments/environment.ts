435c84e65b2778d612ef736840bf39eb9a66be32
Diego Jasso <77365054+dbjasso@users.noreply.github.com>
2022-09-26 21:35:01 -0500

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  base_url: 'http://directus.beatstudio.com.mx/directus/public/beatstudio/',
+  base_url: 'http://localhost/directus/public/beatstudio/',
   paypal_key : 'AVJ9WP8qD0yJwMBZkK8UFK0m4OmG1Obk2l-lM0krkhCx_fJM8-PRFQzwWDrV0vUSnjD7fcJsxWxI7SCd',
   conekta_key: 'key_ENC5Gc1gN4jyXma4kEPWD8Q'
 };
